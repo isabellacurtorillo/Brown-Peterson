7f36ca7cc8fb519b92ced006164ea42cc22985ce
isabella <isabellacurtorillo@gmail.com>
2025-03-31 15:42:07 -0400

    created the trigram generator

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class TaskComponent {
 
+  public trigramLetters: string[] = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"];
+  public trigram: string = "";
+
+  public generateRandomTrigram(trigram: string[]) {
+    this.trigram = "";
+    for (let i = 0; i < 3; i++) {
+      const randomIndex = Math.floor(Math.random() * this.trigramLetters.length);
+      this.trigram += this.trigramLetters[randomIndex];
+    }
+    return this.trigram;
+  }
 }
