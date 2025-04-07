5f520d24bb6c1c1101b41afe05b07ca1eaeb51c1
isabella <isabellacurtorillo@gmail.com>
2025-04-07 15:03:45 -0400

    intial distractor difficulty created

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -7,8 +7,11 @@ import { Component } from '@angular/core';
 })
 export class TaskComponent {
 
+  constructor() { }
+
   public trigramLetters: string[] = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"];
   public trigram: string = "";
+  public formInput: number;
 
   public generateRandomTrigram(trigram: string[]) {
     this.trigram = "";
@@ -21,6 +24,28 @@ export class TaskComponent {
 
   public getTrigram() {
     return this.trigram;
+  }
+
+  public generateRandomOddNumber(){
+    let randomOddNumber = Math.floor(Math.random() * 10) * 2 + 1;
+    return randomOddNumber;
+  }
+
+  public generateRandomEvenNumber(){
+    let randomEvenNumber = Math.floor(Math.random() * 10) * 2;
+    return randomEvenNumber;
+  }
+
+  public distractorDifficulty(formInput: number){
+      for(let i=0; i<formInput; i++){
+        let randomOddNumber = this.generateRandomOddNumber();
+        let randomEvenNumber = this.generateRandomEvenNumber();
+        //display one random number and one random odd number for the number of times the formInput outlines
+        console.log("Random Odd Number: " + randomOddNumber);
+        console.log("Random Even Number: " + randomEvenNumber);
+        
+      }
+
   }
   
 }
