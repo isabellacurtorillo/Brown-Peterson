4f566c9a206d13b1b0f983df8fd550c5407eaf68
Abigail Loeffler <LOEFFLEA@my.erau.edu>
2025-04-07 13:22:17 -0400

    Trigrams data created and Trials and trial class created

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -10,7 +10,7 @@ export class TaskComponent {
   public trigramLetters: string[] = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"];
   public trigram: string = "";
 
-  public generateRandomTrigram(trigram: string[]) {
+  public generateRandomTrigram() {
     this.trigram = "";
     for (let i = 0; i < 3; i++) {
       const randomIndex = Math.floor(Math.random() * this.trigramLetters.length);
