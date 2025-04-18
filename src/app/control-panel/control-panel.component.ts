1a3ab9d33b25e7e3b4b1886ab0dd571deaca5802
isabella <isabellacurtorillo@gmail.com>
2025-04-18 15:14:16 -0400

    updated participant instructions and control panel

--- src/app/control-panel/control-panel.component.ts
+++ src/app/control-panel/control-panel.component.ts
@@ -1,38 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { TaskComponent } from '../task/task.component';
-import { InstructionsComponent } from '../participant-instructions/participant-instructions.component';
 import { ControlPanel } from '../control-panel.model';
-import { FormControl, FormGroup, Validators, FormBuilder, FormArray } from '@angular/forms';
-
-// class trials {
-//   public allTrials: trial[] = [];
-//   public triTask: TaskComponent;
-//   public iTrial: trial;
-
-//   constructor(tnt: number) {
-//     this.triTask = new TaskComponent;
-//       for(let i = 0; i < tnt; i++) {
-//         this.iTrial = new trial(i+1);
-//         this.allTrials.push(this.iTrial);
-//         this.allTrials[i].randTrigram = this.triTask.generateRandomTrigram();
-//         console.log(this.allTrials[i].randTrigram + " " + this.allTrials[i].trialNum);
-//       }
-//   }
-// }
-// //add form for inputs 
-
-// class trial {
-//   public trialNum: number;
-//   public randTrigram: string;
-//   public pi: InstructionsComponent;
-
-//   constructor(tn: number) {
-//     this.trialNum = tn;
-//     this.randTrigram = "";
-//     this.pi = null;
-//   }
-// }
+import { FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-control-panel',
@@ -43,39 +12,17 @@ import { FormControl, FormGroup, Validators, FormBuilder, FormArray } from '@ang
 export class ControlPanelComponent {
 
   public controlpanel: ControlPanel;
+  public validForm: boolean;
   public dynamicForm: FormGroup;
 
   public controlForm: FormGroup = new FormGroup({
-    trials: new FormControl(undefined, [Validators.required, Validators.pattern(/^[0-9]\d*$/), Validators.min(3)]),
-    distractor: new FormControl(undefined, [Validators.required, Validators.pattern(/^[0-9]\d*$/), Validators.min(2)]),
-    distractortwo: new FormControl(undefined, [Validators.required, Validators.pattern(/^[0-9]\d*$/), Validators.min(2)]),
-    distractorthree: new FormControl(undefined, [Validators.required, Validators.pattern(/^[0-9]\d*$/), Validators.min(2)]),
+    trials: new FormControl(undefined, [Validators.required, Validators.min(5)]),
+    distractor: new FormControl(undefined, Validators.required),
+    distractortwo: new FormControl(undefined, Validators.required),
+    distractorthree: new FormControl(undefined, Validators.required)
   });
 
-  constructor(private router: Router, private fb: FormBuilder) {
-    this.dynamicForm = this.fb.group({
-      bulkRuns: this.fb.array([])
-    });
-  }
-
-
-
-  public get bulkRuns(): FormArray {
-    return this.dynamicForm.get('bulkRuns') as FormArray;
-  }
-
-
-  public addBulkRun() {
-    this.bulkRuns.push(this.fb.group({
-      quantity: [],
-      code: []
-    }));
-    console.log(this.dynamicForm);
-  }
-
-  public removeBulkRun(index: number) {
-    this.bulkRuns.removeAt(index);
-  }
+  constructor(private router: Router){ }
 
   ngpInit() {
 
@@ -83,7 +30,15 @@ export class ControlPanelComponent {
 
   onSubmit() {
     console.log(this.controlForm);
-    console.log(this.dynamicForm);
+    this.router.navigate(['instructions']);
+    
+
+    if(this.controlForm.value.distractor == this.controlForm.value.distractortwo || this.controlForm.value.distractorThree == this.controlForm.value.distractortwo || this.controlForm.value.distractor == this.controlForm.value.distractorthree){
+      console.log("Distractors cannot be the same");
+      this.validForm = false;
+      return this.controlForm.invalid;
+    }
+
     if(this.controlForm.valid){
       const numberOfTrials = this.controlForm.value.trials;
       const distractorOne = this.controlForm.value.distractor;
@@ -99,106 +54,5 @@ export class ControlPanelComponent {
     }
   }
 
-  isDisabled():boolean {
-    if(this.controlForm.valid) {
-      if((this.controlForm.value.distractor !== this.controlForm.value.distractortwo) && (this.controlForm.value.distractor !== this.controlForm.value.distractorthree) && (this.controlForm.value.distractortwo !== this.controlForm.value.distractorthree)) {
-        return false;
-      }
-    }
-    return true;
-  }
+
 }
-//   constructor(private router: Router ) {
-//     // const now = new Date();
-//     // this.controlpanel.date = now.toLocaleDateString();
-//     // this.controlpanelinvalidTrialsAmount = true;
-//     // this.invalidDtq1 = true;
-//     // this.invalidDtq2 = true;
-//     // this.invalidDtq3 = true;
-//     // console.log(this.date)
-//    }
-
-//   ngonInit() {
-//     this.controlForm.formGroup({
-//       sTrialsAmount: '',
-//       sDtq1: '',
-//       sDtq2: '',
-//   });
-// }
-//   public checkQuantityEmpty(q: string, n: number) {
-//       switch (n) {
-//           case 1: 
-//             if(q.length == 0 || this.containsOnlyNumbers(q) == false) {
-//               this.controlpanel.invalidTrialsAmount = true;
-//             }
-//             else {
-//               this.controlpanel.nQuant = parseInt(q);
-//               if(this.controlpanel.nQuant <= 2) {
-//                 this.controlpanel.invalidTrialsAmount = true;
-//                 console.log(this.controlpanel.errorMessage);
-//               }else {
-//                 this.controlpanel.nTrialsAmount = this.controlpanel.nQuant;
-//                 this.controlpanel.invalidTrialsAmount = false;
-//               }
-//             }
-//               break;
-//           case 2: 
-//             if(q.length == 0 || this.containsOnlyNumbers(q) == false) {
-//               this.controlpanel.invalidDtq1 = true;
-//             }
-//             else {
-//               this.nQuant = parseInt(q);
-//               if(this.nQuant <= 2 || this.nQuant == this.nDtq2 || this.nQuant == this.nDtq3) {
-//                 this.invalidDtq1 = true;
-//                 console.log(this.errorMessage);
-//               }else {
-//                 this.nDtq1 = this.nQuant;
-//                 this.invalidDtq1 = false;
-//               }
-//             }
-//               break;
-//           case 3:
-//             if(q.length == 0 || this.containsOnlyNumbers(q) == false) {
-//               this.invalidDtq2 = true;
-//             }
-//             else {
-//               this.nQuant = parseInt(q);
-//               if(this.nQuant <= 2 || this.nDtq1 == this.nQuant || this.nQuant == this.nDtq3 ) {
-//                 this.invalidDtq2 = true;
-//                 console.log(this.errorMessage);
-//               }else {
-//                 this.nDtq2 = this.nQuant;
-//                 this.invalidDtq2 = false;
-//               }
-//             }
-//               break;
-//           case 4:
-//             if(q.length == 0 || this.containsOnlyNumbers(q) == false) {
-//               this.invalidDtq3 = true;
-//             }
-//             else {
-//               this.nQuant = parseInt(q);
-//               if(this.nQuant <= 2 || this.nQuant == this.nDtq2 || this.nQuant == this.nDtq1) {
-//                 this.invalidDtq3 = true;
-//                 console.log(this.errorMessage);
-//               }else {
-//                 this.nDtq3 = this.nQuant;
-//                 this.invalidDtq3 = false;
-//               }
-//             }
-//               break;
-//         }
-//   }
-
-//   public containsOnlyNumbers(q: string):boolean { return /^\d+$/.test(q); 
-  
-//   public onSubmit() {
-//     console.log(this.nTrialsAmount + " " + this.nDtq1 + " " + this.nDtq2 + " " + this.nDtq3);
-//     const tt = new trials(this.nTrialsAmount);
-//     tt.allTrials[0].pi = new InstructionsComponent(this.router);
-//   }
-
-// }
-
-
-
